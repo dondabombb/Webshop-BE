6147e4b374145cab6f444d6eea96d41ee75e75f5
s1135796 <s1135796@student.hsleiden.nl>
2023-01-06 21:42:23 +0100

    commit

---
+++ src/test/java/nl/hsleiden/WebshopBE/WebshopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.WebshopBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
