413784462719e06c7fbf3a84112f5250e194df36
Don Vu <s1135796@student.hsleiden.nl>
2025-04-17 02:04:30 +0200

    for prod

--- src/main/java/nl/hsleiden/WebshopBE/config/SecurityConfig.java
+++ src/main/java/nl/hsleiden/WebshopBE/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://104.248.84.219"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
